144f32b23ac4b0524186032f8c70406e99486824
Naresh17025 <101312396+Naresh17025@users.noreply.github.com>
2022-03-30 20:18:19 +0530

    update documentation

--- 3_Implementation/documentation/html/search/all_5.js
+++
@@ -1,5 +0,0 @@
-var searchData=
-[
-  ['status_0',['status',['../bus__operations_8h.html#a23939106a6e2cda8545e50fc0635d0eb',1,'bus_operations.h']]],
-  ['status_5f1_1',['status_1',['../bus__operations_8h.html#ae935599c9ecb345c3a7f84906246dfdd',1,'bus_operations.h']]]
-];
--- 3_Implementation/documentation/html/search/functions_4.js
+++
@@ -1,5 +0,0 @@
-var searchData=
-[
-  ['read_5fn0_0',['read_n0',['../bus__operations_8h.html#afef94f8ecf3ae433dce5e3d4618033a2',1,'bus_operations.h']]],
-  ['read_5fname_1',['read_name',['../bus__operations_8h.html#a9876bf04274fd00a2f29a212e5924e94',1,'bus_operations.h']]]
-];
